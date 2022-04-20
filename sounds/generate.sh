1e4550ea85ab9e53380882eaafadbbc78ec1584d
Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2022-04-20 10:39:32 +0200

    Add files via upload

--- sounds/generate.sh
+++ sounds/generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for number in {1..9}
+for number in {0..9}
 do
 echo "$number "
 ffmpeg -i default.mp3 -af volume=0.0$number default$number.wav
@@ -11,7 +11,7 @@ ffmpeg -i fart.mp3 -af volume=0.0$number fart$number.wav
 done
 
 
-for number in {10..100}
+for number in {10..99}
 do
 echo "$number "
 ffmpeg -i default.mp3 -af volume=0.$number default$number.wav
@@ -20,4 +20,12 @@ ffmpeg -i business.mp3 -af volume=0.$number business$number.wav
 ffmpeg -i klingel.mp3 -af volume=0.$number klingel$number.wav
 ffmpeg -i fart.mp3 -af volume=0.$number fart$number.wav
 done
+
+ffmpeg -i default.mp3 -af volume=1 default100.wav
+ffmpeg -i classic.mp3 -af volume=1 classic100.wav
+ffmpeg -i business.mp3 -af volume=1 business100.wav
+ffmpeg -i klingel.mp3 -af volume=1 klingel100.wav
+ffmpeg -i fart.mp3 -af volume=1 fart100.wav
+
+
 exit 0
