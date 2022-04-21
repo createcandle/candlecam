0c18e2f7d1354863bd2623ec71ed08ae38b3cb39
Candle - the privacy friendly smart home <39312291+createcandle@users.noreply.github.com>
2022-04-21 16:50:26 +0200

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,12 +1,20 @@
 #!/bin/bash -e
 
+echo "in package.sh"
+pwd
+which python3
+which pip3
+pip3 install --user --upgrade pip
+
+
 version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
+
+export PYTHONIOENCODING=utf8
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export LD_LIBRARY_PATH="$HOME/.local/lib:/usr/local/lib:$LD_LIBRARY_PATH" LIBRARY_PATH="$HOME/.local/lib/" CFLAGS="-I$HOME/.local/include"
 
 # Setup environment for building inside Dockerized toolchain
 [ $(id -u) = 0 ] && umask 0
-
-# Clean up from previous releases
-rm -rf *.tgz *.sha256sum package SHA256SUMS lib
 
 if [ -z "${ADDON_ARCH}" ]; then
   TARFILE_SUFFIX=
@@ -15,30 +23,37 @@ else
   TARFILE_SUFFIX="-${ADDON_ARCH}-v${PYTHON_VERSION}"
 fi
 
+# Clean up from previous releases
+echo "removing old files"
+rm -rf *.tgz *.shasum package SHA256SUMS lib
+
+# Prep new package
+echo "creating package"
 mkdir -p lib package
-mkdir -p package/photos
 
-# Pull down Python dependencies
-export READTHEDOCS=True
-pip3 install -r requirements.txt -t lib --no-binary :all: --prefix ""
+pip3 install -r requirements.txt -t lib --no-binary :all: --prefix "" --default-timeout=100
 
-cp *.py manifest.json LICENSE README.md package/
-cp -r pkg lib css images js views package/
+# Remove local cffi so that the globally installed version doesn't clash
+rm -rf ./lib/cffi*
+
+# Put package together
+cp -r lib pkg LICENSE manifest.json *.py README.md sounds css js images views package/
 find package -type f -name '*.pyc' -delete
 find package -type f -name '._*' -delete
+find package -type d -empty -delete
 
+# Generate checksums
 echo "generating checksums"
 cd package
 find . -type f \! -name SHA256SUMS -exec shasum --algorithm 256 {} \; >> SHA256SUMS
 cd -
 
-
-
 # Make the tarball
 echo "creating archive"
-TARFILE="candlecam-${version}.tgz"
+TARFILE="candlecam-${version}${TARFILE_SUFFIX}.tgz"
 tar czf ${TARFILE} package
 
+# create shasum of final tar
+echo "creating shasum of final tar archive"
 shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
 cat ${TARFILE}.sha256sum
-
