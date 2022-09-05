1b60aba9090d0fc522dd97d561e2da58ff2af3c4
flatsiedatsie <unfold@gmail.com>
2022-09-05 14:55:39 +0200

    Update package.sh

--- package.sh
+++ package.sh
@@ -2,9 +2,14 @@
 
 echo "in package.sh"
 pwd
+echo "which python3:"
 which python3
+echo "which pip3:"
 which pip3
+echo
+echo "Upgrading pip:"
 pip3 install --user --upgrade pip
+python3 -m pip install --upgrade pip
 #/usr/local/bin/python3.9 -m pip install --upgrade pip
 
 version=$(grep '"version"' manifest.json | cut -d: -f2 | cut -d\" -f2)
@@ -13,6 +18,8 @@ export PYTHONIOENCODING=utf8
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export LD_LIBRARY_PATH="$HOME/.local/lib:/root/.local/bin:/usr/local/lib:$LD_LIBRARY_PATH" LIBRARY_PATH="$HOME/.local/lib/" CFLAGS="-I$HOME/.local/include"
 
+echo
+echo "setting up environment variables for dockerized toolchain"
 # Setup environment for building inside Dockerized toolchain
 [ $(id -u) = 0 ] && umask 0
 
@@ -36,15 +43,24 @@ mkdir -p lib package
 export READTHEDOCS=True 
 export NOGUI="1"
 
+
+
 #NOGUI=1 pip3 install picamera2
 #wget http://ftp.nl.debian.org/debian/pool/main/libc/libcap2/libcap-dev_2.44-1_armhf.deb
 #dpkg -i libcap-dev_2.44-1_armhf.deb
+echo
+echo "installing numpy"
 pip3 install numpy
 #pip3 install --upgrade libpcap PiDNG piexif pillow simplejpeg v4l2-python3 python-prctl
 #pip3 install --upgrade --no-deps picamera2
 
+echo
+echo "installing requirements"
+echo
 pip3 install -r requirements.txt -t lib --no-binary :all: --prefix "" #--default-timeout=100
 
+echo
+echo "install picamera2"
 pip3 install -r picamera2 -t lib  --no-deps --no-binary :all: --prefix ""
 # Remove local cffi so that the globally installed version doesn't clash
 rm -rf ./lib/cffi*
